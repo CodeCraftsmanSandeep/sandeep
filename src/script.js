c03b9e123320c8744eec08541b087da9f6224134
Chekkala Sandeep Reddy <sandeep.chekkala.wh@gmail.com>
2025-02-27 16:13:59 +0530

    mobile version not supported

--- src/script.js
+++ src/script.js
@@ -1,15 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Simulate visitor count using local storage (this can be replaced with actual visitor counter logic)
-    let visitorCount = localStorage.getItem("visitorCount");
-    if (!visitorCount) {
-        visitorCount = 1;
-    } else {
-        visitorCount = parseInt(visitorCount) + 1;
-    }
-    localStorage.setItem("visitorCount", visitorCount);
-    document.getElementById("visitorCount").innerText = visitorCount;
-});
-
 if (window.innerWidth < 768) {
     window.location.href = "mobile-not-supported.html";
   }
